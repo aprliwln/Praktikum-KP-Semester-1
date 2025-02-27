e05964509db4226f5f1c65218df2b4c2d742e2ce
Eiyprel <181449046+aprliwln@users.noreply.github.com>
2025-02-27 18:51:41 +0700

    Update and rename bermain_lampu_kandang.c to bermain_lampu_kandang/bermain_lampu_kandang.c

--- bermain_lampu_kandang/bermain_lampu_kandang.c
+++ bermain_lampu_kandang/bermain_lampu_kandang.c
@@ -2,25 +2,19 @@
 
 int main(){
     int lampu[101];
-
-    // Pada mulanya, seluruh lampu dalam kondisi mati.
+    
     for (int i = 1; i <= 100; i++) {
         lampu[i] = 0;
     }
 
-    // Untuk setiap jam (pukul p),
     for (int p = 1 ; p <= 10; p++) {
-        // untuk setiap lampu nomor i,
         for (int i = 1; i <=100; i++ ) {
-            // jika i merupakan kelipatan p,
             if (i % p == 0) {
-                // tekan saklarnya.
                 lampu[i] = !lampu[i];
             }
         }
     }
 
-    // Hitung banyaknya lampu yang menyala,
     int totalnyala = 0;
     for (int i = 1; i <= 100; i++){
         if (lampu[i] == 1)
@@ -31,4 +25,5 @@ int main(){
     }
 
     printf("%d" ,totalnyala);
+    return 0;
 }
