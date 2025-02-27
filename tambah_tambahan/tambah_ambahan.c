e56fea628729ac2bd3900d8dd195f9419ea0ecfe
Eiyprel <181449046+aprliwln@users.noreply.github.com>
2025-02-27 20:44:54 +0700

    Update and rename Tambahan.c to tambah_ambahan.c

--- tambah_tambahan/tambah_ambahan.c
+++ tambah_tambahan/tambah_ambahan.c
@@ -1,26 +1,3 @@
-/*
-Determine if one of them is the sum of the other two.
-
-Input:
-7 (l0op)
-1 4 3
-2 5 8
-9 11 20
-0 0 0
-20 20 20
-4 12 3
-15 7 8
-
-Output:
-YES
-NO
-YES
-YES
-NO
-NO
-YES
-*/
-
 #include <stdio.h>
 
 int main(){
@@ -37,5 +14,5 @@ int main(){
                 printf("NO\n");
                 }
     } 
-return 0;
+    return 0;
 }
